c1e32d341b2fee6306b41914e9a10424ed5124ed
Kislov Dmitry <kislov@easydan.com>
2019-06-23 13:57:59 +1000

    Works locally

--- test.py
+++ test.py
@@ -393,12 +393,13 @@ class LimitedMemBackendTests(unittest.TestCase):
 
 class LimitedFileBackenTests(LimitedMemBackendTests):
     def setUp(self):
-        self.backend_lfu = LimitedFileBackend('limited_lfu', cache_size=3, algorithm='lfu')
-        self.backend_mfu = LimitedFileBackend('limited_mfu', cache_size=3, algorithm='mfu')
+        self.backend_lfu = LimitedFileBackend(TEMP_FILENAME, cache_size=3, algorithm='lfu')
+        self.backend_mfu = LimitedFileBackend(TEMP_FILENAME, cache_size=3, algorithm='mfu')
 
     def tearDown(self):
         self.backend_lfu.close()
         self.backend_mfu.close()
+        FileBackendTests.clear_storage()
 
 
 class LimitedCacheToMemTests(BaseCacheToMemTests):
